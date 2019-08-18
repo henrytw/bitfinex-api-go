3830b2c6ddef16c8abacd69f14c23faef3d18498
Heng-Ruey Hsu <henryhsu@chromium.org>
2019-08-18 19:02:14 +0800

    unified nonce

--- utils/nonce.go
+++ utils/nonce.go
@@ -22,12 +22,12 @@ type EpochNonceGenerator struct {
 // key and as such needs to be synchronised with other instances using the same
 // key in order to avoid race conditions.
 func (u *EpochNonceGenerator) GetNonce() string {
-	return strconv.FormatUint(atomic.AddUint64(&u.nonce, 1), 10)
+	return uint64(time.Now().UnixNano()) / 1000000
 }
 
 func NewEpochNonceGenerator() *EpochNonceGenerator {
 	return &EpochNonceGenerator{
-		nonce: uint64(time.Now().Unix()) * 1000,
+		nonce: uint64(time.Now().UnixNano()) / 1000000,
 	}
 }
 
@@ -45,5 +45,5 @@ func init() {
 // key and as such needs to be synchronised with other instances using the same
 // key in order to avoid race conditions.
 func GetNonce() string {
-	return strconv.FormatUint(atomic.AddUint64(&nonce, 1), 10)
+	return uint64(time.Now().UnixNano()) / 1000000
 }
